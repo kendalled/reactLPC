b9a47d5f16ff918680d3b98c4a9920aa8e6f312d
Kendall Jackson <kendallkj00@gmail.com>
2019-04-02 14:30:43 -0400

    Changed header text and added nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <NavBar></NavBar>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
